0594d45920c38e51ed64c1b6cfb6b024868a82f8
Sky <Sky.Rivera89@gmail.com>
2021-09-09 20:18:39 +0430

    Linear Search
    Linear search algorithm

--- Search.py
+++ Search.py
@@ -27,6 +27,37 @@ def main():
       print(Fore.RED + "Error! \nPlease choose a valin number.")
       main()
 
+################################################ LINEAR ################################################
+
+def linearSearch():
+    os.system('cls')
+    print(Fore.YELLOW + "Linear Search\n\n")
+    print(Style.RESET_ALL)
+
+    array = input("Enter the list : ")
+    array = array.split()
+    array = [int(x) for x in array]
+
+    print(Fore.CYAN)
+    key = int(input("Enter the key : "))
+
+    index = Linear_Search(array, key)
+
+    if index < 0:
+        print(Fore.RED + "{} was not found.".format(key))
+        print(Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "{} was faound at index {}.".format(key, index))
+        print(Style.RESET_ALL)
+
+
+def Linear_Search(alist, key):
+
+    for i in range(len(alist)):
+        if alist[i] == key:
+            return i
+    return -1
+
 ################################################ BODY ################################################
 
 os.system('cls')
