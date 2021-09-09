650db705c9e7ee38d77538dc554319dbe25c0854
Sky <Sky.Rivera89@gmail.com>
2021-09-09 20:17:25 +0430

    Main and Body
    Code of main body and a main function to point to the main.

--- Search.py
+++ Search.py
@@ -1,1 +1,49 @@
-# START OF PROJECT
+import colorama
+import os
+from pyfiglet import Figlet
+from colorama import Fore, Back, Style
+
+################################################ MAIN ################################################
+
+def main():
+
+    print(Style.RESET_ALL)
+
+    option = input("Choose your option : ")
+
+    if option == "1":
+        linearSearch()
+
+    elif option == "2":
+        binarySearch()
+
+    elif option == "3":
+        binarySearchRecursion()
+
+    elif option == "4":
+        binarySearchTree()
+
+    else:
+      print(Fore.RED + "Error! \nPlease choose a valin number.")
+      main()
+
+################################################ BODY ################################################
+
+os.system('cls')
+
+banner = Figlet(font='banner3-D')
+
+print(Fore.RED)
+print(banner.renderText("Searchs"))
+print(Style.RESET_ALL)
+
+print("Lists of searchs : ")
+
+searchsArray = ["Linear Search", "Binary Search", "Binary Search Recursion", "Binary Search Tree"]
+listNumbers = "[{}] : "
+
+for i in range(len(searchsArray)):
+    print(Fore.GREEN)
+    print(listNumbers.format(i+1) + Fore.LIGHTBLACK_EX + searchsArray[i])
+
+main()
