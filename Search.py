209eb5af419d32896a7f20bff27bbcf639f2a4a5
Sky <Sky.Rivera89@gmail.com>
2021-09-11 20:27:52 +0430

    Update binary search tree class
    Preorders, Inorders and Postorders.

--- Search.py
+++ Search.py
@@ -74,6 +74,33 @@ class BinarySearchTree_Node:
             return False
         return self.right.Exists(val)
 
+    def Preorder(self, vals):
+        if self.val is not None:
+            vals.append(self, val)
+        if self.left is not None:
+            self.left.Preorder(vals)
+        if self.right is not None:
+            self.right.Preorder(vals)
+        return vals
+
+    def Inorder(self, vals):
+        if self.left is not None:
+            self.left.inorder(vals)
+        if self.val is not None:
+            vals.append(self.val)
+        if self.right is not None:
+            self.right.inorder(vals)
+        return vals
+
+    def Postorder(self, vals):
+        if self.left is not None:
+            self.left.postorder(vals)
+        if self.right is not None:
+            self.right.postorder(vals)
+        if self.val is not None:
+            vals.append(self.val)
+        return vals
+
 ################################################ MAIN ################################################
 
 def main():
